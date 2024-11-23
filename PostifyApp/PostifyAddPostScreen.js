028389f73999281fe1111a9a26a0741aff4c2ff1
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-11-23 21:18:12 +1100

    Update PostifyAddPostScreen.js

--- PostifyApp/PostifyAddPostScreen.js
+++ PostifyApp/PostifyAddPostScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Pressable, Image, StyleSheet, Alert } from 'react-native';
 import { db, auth } from './firebase';
-import { Ionicons } from '@expo/vector-icons';
 import { collection, addDoc, Timestamp } from 'firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
 
