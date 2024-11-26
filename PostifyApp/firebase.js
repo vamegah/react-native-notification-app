d59e494bbdfa44e9e4bdf887a1081b88df35deec
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-11-27 08:26:03 +1100

    Update firebase.js
    Removed the values that are repopulated

--- PostifyApp/firebase.js
+++ PostifyApp/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByvHyTPvwSD5iI9gFMeKk89eCoL3P__Ss",
-  authDomain: "reactnative-push-notific-e871b.firebaseapp.com",
-  projectId: "reactnative-push-notific-e871b",
-  storageBucket: "reactnative-push-notific-e871b.appspot.com",
-  messagingSenderId: "98427882719",
-  appId: "1:98427882719:web:891247ae88d46914eaed6a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
